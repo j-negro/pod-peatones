a05e81c01296eb2624244aa8e505128fb5df7969
gonzaloRossin <gonzarossin@gmail.com>
2022-10-25 20:45:36 -0300

    feat: add firstQueryColator implementation

--- api/src/main/java/ar/edu/itba/pod/api/mappers/FirstQueryMapper.java
+++ api/src/main/java/ar/edu/itba/pod/api/mappers/FirstQueryMapper.java
@@ -22,7 +22,7 @@ public class FirstQueryMapper implements Mapper<String, Reading, String, Long>, 
         IMap<Integer, Sensor> map = hazelcastInstance.getMap("sensors");
 
         Sensor sensor = map.get(reading.getSensorId());
-        if(sensor.getStatus().compareTo("A") == 0 )
+        if(sensor.getStatus().equals("A") )
             context.emit(sensor.getDescription(), (long) reading.getHourlyCounts());
     }
 
