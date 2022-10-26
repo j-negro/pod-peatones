acf22c7e22696f554ef1c76ab8dcb7703709bb0f
Juan Manuel Negro <jnegro@itba.edu.ar>
2022-10-26 20:57:58 -0300

    Add SecondQueryCombiner

--- api/src/main/java/ar/edu/itba/pod/api/reducers/SecondQueryReducerFactory.java
+++ api/src/main/java/ar/edu/itba/pod/api/reducers/SecondQueryReducerFactory.java
@@ -4,29 +4,26 @@ import ar.edu.itba.pod.api.models.Pair;
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
-public class SecondQueryReducerFactory implements ReducerFactory<Integer, Pair<Boolean, Integer>, Pair<Integer, Integer>> {
+public class SecondQueryReducerFactory implements ReducerFactory<Integer, Pair<Integer, Integer>, Pair<Integer, Integer>> {
     @Override
-    public Reducer<Pair<Boolean, Integer>, Pair<Integer, Integer>> newReducer(Integer integer) {
+    public Reducer<Pair<Integer, Integer>, Pair<Integer, Integer>> newReducer(Integer integer) {
         return new SecondQueryReducer();
     }
 
-    private class SecondQueryReducer extends Reducer<Pair<Boolean, Integer>, Pair<Integer, Integer>> {
+    private class SecondQueryReducer extends Reducer<Pair<Integer, Integer>, Pair<Integer, Integer>> {
 
         private int weekdayCount = 0;
         private int weekendCount = 0;
 
         @Override
-        public void reduce(Pair<Boolean, Integer> weekendCountPair) {
-            if (weekendCountPair.getFirst()) {
-                weekendCount += weekendCountPair.getSecond();
-            } else {
-                weekdayCount += weekendCountPair.getSecond();
-            }
+        public void reduce(Pair<Integer, Integer> weekendCountPair) {
+            weekdayCount += weekendCountPair.getFirst();
+            weekendCount += weekendCountPair.getSecond();
         }
 
         @Override
         public Pair<Integer, Integer> finalizeReduce() {
-            return new Pair<Integer, Integer>(weekdayCount, weekendCount);
+            return new Pair<>(weekdayCount, weekendCount);
         }
     }
 }
