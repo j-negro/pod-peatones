d5bad8c450bc59d4c4afcd778dc7af39688b6437
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-10-26 14:35:52 -0300

    Implement mapper from sensor count to billion list

---
+++ api/src/main/java/ar/edu/itba/pod/api/mappers/FifthQueryMapper.java
@@ -0,0 +1,27 @@
+package ar.edu.itba.pod.api.mappers;
+
+import ar.edu.itba.pod.api.models.Pair;
+import ar.edu.itba.pod.api.models.Reading;
+import ar.edu.itba.pod.api.models.Sensor;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Context;
+import com.hazelcast.mapreduce.Mapper;
+
+public class FifthQueryMapper implements Mapper<String, Long, Long, String>, HazelcastInstanceAware {
+
+    private HazelcastInstance hazelcastInstance;
+
+    @Override
+    public void map(String s, Long count, Context<Long, String> context) {
+        long billionCount = count / 1000000;
+        if(billionCount > 0)
+            context.emit(billionCount, s);
+    }
+
+    @Override
+    public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+}
