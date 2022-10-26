2ad339691978d37a630bec80db08d3f5e309e144
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-10-26 19:52:28 -0300

    Fix collator

--- api/src/main/java/ar/edu/itba/pod/api/collators/ForthQueryCollator.java
+++ api/src/main/java/ar/edu/itba/pod/api/collators/ForthQueryCollator.java
@@ -4,9 +4,16 @@ package ar.edu.itba.pod.api.collators;
 import ar.edu.itba.pod.api.models.Pair;
 import com.hazelcast.mapreduce.Collator;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class ForthQueryCollator implements Collator<Map.Entry<String, Pair<String, Double>>, SortedSet<Map.Entry<String, Pair<String, Double>>>> {
+
+    private final Comparator<Map.Entry<String, Pair<String, Double>>> COMPARATOR = Comparator
+            .comparingDouble((Map.Entry<String, Pair<String, Double>> o) -> o.getValue().getSecond()).reversed()
+            .thenComparing(Map.Entry::getKey);
 
     private final long n;
 
@@ -17,10 +24,10 @@ public class ForthQueryCollator implements Collator<Map.Entry<String, Pair<Strin
     @Override
     public SortedSet<Map.Entry<String, Pair<String, Double>>> collate(Iterable<Map.Entry<String, Pair<String, Double>>> values) {
         SortedSet<Map.Entry<String, Pair<String, Double>>> set =
-                new TreeSet<>(Map.Entry.comparingByValue(Comparator.comparingDouble(Pair::getSecond)));
+                new TreeSet<>(COMPARATOR);
         int i = 0;
         for (Map.Entry<String, Pair<String, Double>> entry : values) {
-            if (i < n) break;
+            // if (i < n) break;
             set.add(entry);
             i++;
         }
